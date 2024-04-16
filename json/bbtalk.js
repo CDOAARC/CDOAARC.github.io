b06aca4ac803c0b9f1595480eecc83264cbe3b00
CDOA <852435668@qq.com>
2024-04-16 15:20:03 +0800

    Site updated: 2024-04-16 15:19:59

---
+++ json/bbtalk.js
@@ -0,0 +1,10 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>if (document.querySelector('#bber-talk')) {
+    var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical', // 垂直切换选项
+      loop: true,
+      autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true
+    },
+    });
+  }
